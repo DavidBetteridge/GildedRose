be6a80973b996b4389fc4a124e85595fcc9fb25c
David Betteridge <DavidaBetteridge@gmail.com>
2020-03-11 16:34:42 +0000

    Refactor

--- csharpcore/GildedRose.cs
+++ csharpcore/GildedRose.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 
 namespace csharpcore
 {
@@ -12,82 +13,42 @@ namespace csharpcore
 
         public void UpdateQuality()
         {
-            for (var i = 0; i < Items.Count; i++)
+            foreach (var item in Items)
             {
-                if (Items[i].Name != "Aged Brie" && Items[i].Name != "Backstage passes to a TAFKAL80ETC concert")
-                {
-                    if (Items[i].Quality > 0)
-                    {
-                        if (Items[i].Name == "Conjured")
-                        {
-                            Items[i].Quality = Items[i].Quality - 2;
-                        }
-                        else if (Items[i].Name != "Sulfuras, Hand of Ragnaros")
-                        {
-                            Items[i].Quality = Items[i].Quality - 1;
-                        }
-                    }
-                }
-                else
-                {
-                    if (Items[i].Quality < 50)
-                    {
-                        Items[i].Quality = Items[i].Quality + 1;
-
-                        if (Items[i].Name == "Backstage passes to a TAFKAL80ETC concert")
-                        {
-                            if (Items[i].SellIn < 11)
-                            {
-                                if (Items[i].Quality < 50)
-                                {
-                                    Items[i].Quality = Items[i].Quality + 1;
-                                }
-                            }
-
-                            if (Items[i].SellIn < 6)
-                            {
-                                if (Items[i].Quality < 50)
-                                {
-                                    Items[i].Quality = Items[i].Quality + 1;
-                                }
-                            }
-                        }
-                    }
-                }
-
-                if (Items[i].Name != "Sulfuras, Hand of Ragnaros")
-                {
-                    Items[i].SellIn = Items[i].SellIn - 1;
-                }
-
-                if (Items[i].SellIn < 0)
-                {
-                    if (Items[i].Name != "Aged Brie")
-                    {
-                        if (Items[i].Name != "Backstage passes to a TAFKAL80ETC concert")
-                        {
-                            if (Items[i].Quality > 0)
-                            {
-                                if (Items[i].Name != "Sulfuras, Hand of Ragnaros")
-                                {
-                                    Items[i].Quality = Items[i].Quality - 1;
-                                }
-                            }
-                        }
-                        else
-                        {
-                            Items[i].Quality = Items[i].Quality - Items[i].Quality;
-                        }
-                    }
-                    else
-                    {
-                        if (Items[i].Quality < 50)
-                        {
-                            Items[i].Quality = Items[i].Quality + 1;
-                        }
-                    }
-                }
+                UpdateItem(item);
             }
         }
+
+        private void UpdateItem(Item item)
+        {
+            var qualityUpdaterForItem = QualityUpdaterForItem(item);
+            var sellInUpdaterForItem = SellInUpdaterForItem(item);
+
+            item.Quality = qualityUpdaterForItem(item.Quality);
+            item.SellIn = sellInUpdaterForItem(item.SellIn);
+        }
+
+        private Func<int, int> SellInUpdaterForItem(Item item) =>
+            item.Name switch
+            {
+                "Sulfuras, Hand of Ragnaros" => (Quality) => Quality,
+                _ => (SellIn) => SellIn - 1,
+            };
+
+        private Func<int, int> QualityUpdaterForItem(Item item) =>
+            item.Name switch
+            {
+                "Aged Brie" when item.Quality >= 50 => (Quality) => Quality,
+                "Aged Brie" => (Quality) => Quality + 1,
+                "Backstage passes to a TAFKAL80ETC concert" when item.SellIn > 10 => (Quality) => Quality + 1,
+                "Backstage passes to a TAFKAL80ETC concert" when item.SellIn > 5 => (Quality) => Quality + 2,
+                "Backstage passes to a TAFKAL80ETC concert" when item.SellIn > 0 => (Quality) => Quality + 3,
+                "Backstage passes to a TAFKAL80ETC concert" => (Quality) => 0,
+                "Sulfuras, Hand of Ragnaros" => (Quality) => Quality,
+                _ when item.Quality <= 0 => (Quality) => Quality,
+                "Conjured" => (Quality) => Math.Max(0, Quality - 2),
+                _ when item.SellIn <= 0 => (Quality) => Math.Max(0, Quality - 2),
+                _ => (Quality) => Math.Max(0, Quality - 1),
+            };
     }
 }
